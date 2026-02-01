384eb3f8604a300b7d47cfb3e5105d763d0b16a1
Juncen Zhou <115336197+higherbros@users.noreply.github.com>
2026-02-01 20:52:15 +0800

    align node version (#12)

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
-import { builtinModules } from "node:module";
-import { defineConfig } from "vite";
+import { builtinModules } from 'node:module';
+import { defineConfig } from 'vite';
 
 const external = [
   ...builtinModules,
-  ...builtinModules.map((module) => `node:${module}`)
+  ...builtinModules.map((module) => `node:${module}`),
 ];
 
 export default defineConfig({
   build: {
-    target: "node22",
+    target: 'node20',
     lib: {
-      entry: "src/cli.ts",
-      formats: ["es"],
-      fileName: "cli"
+      entry: 'src/cli.ts',
+      formats: ['es'],
+      fileName: 'cli',
     },
-    outDir: "dist",
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       external,
       output: {
-        banner: "#!/usr/bin/env node"
-      }
-    }
-  }
+        banner: '#!/usr/bin/env node',
+      },
+    },
+  },
 });
